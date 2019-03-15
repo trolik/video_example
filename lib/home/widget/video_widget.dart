7f18d34e378c87780de4f1ed1dd0b347ad0090c3
Sergey Gaidaichuk <gaydaychuk@restream.rt.ru>
2019-03-15 16:22:43 +0300

    Image was positioned inside card in strange way, so i fixed it in strange way (need to refactor later)

--- lib/home/widget/video_widget.dart
+++ lib/home/widget/video_widget.dart
@@ -17,29 +17,38 @@ class _VideoRowState extends State<VideoRow> {
   @override
   Widget build(BuildContext context) {
     return Card(
-      color: Colors.grey[200],
+        margin: EdgeInsets.all(10),
+        color: Colors.grey[200],
         child: InkWell(
           child: Padding(
-              padding: EdgeInsets.all(4),
-              child: _buildListTile()
+              padding: EdgeInsets.all(0),
+              child: SizedBox(
+                height: 120,
+                child: _buildListTile()
+              )
           ),
           onTap: () {
 
           },
         )
-      );
+    );
   }
 
   ListTile _buildListTile() {
     return ListTile(
-      title: Text(widget.video.videoPath),
-      subtitle: Text(widget.video.thumbPath),
-      leading: Image.file(
-        File(widget.video.thumbPath),
-        width: 100,
-        height: 100,
-        fit: BoxFit.cover
-      )
+      title: Text(widget.video.date.toLocal().toString()),
+      subtitle: Text("Duration: ${widget.video.duration} seconds"),
+      leading: Container(padding: EdgeInsets.fromLTRB(0, 40, 0, 0), child: _image())
+    );
+  }
+
+  Image _image() {
+    return Image.file(
+      File(widget.video.thumbPath),
+      width: 100,
+      height: 100,
+      alignment: Alignment.center,
+      fit: BoxFit.cover
     );
   }
 }
