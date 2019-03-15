2b8439658f9983199c7f54af31d3bd2e64d6c9d1
Sergey Gaidaichuk <gaydaychuk@restream.rt.ru>
2019-03-15 17:32:23 +0300

    Add method channel to share screen. Now we can share String

--- android/app/src/main/kotlin/com/trolik/video_example/MainActivity.kt
+++ android/app/src/main/kotlin/com/trolik/video_example/MainActivity.kt
@@ -1,13 +1,48 @@
 package com.trolik.video_example
 
+import android.content.Intent
 import android.os.Bundle
-
 import io.flutter.app.FlutterActivity
+import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugins.GeneratedPluginRegistrant
 
+
+
 class MainActivity: FlutterActivity() {
+  private val CHANNEL = "samples.flutter.io/share_file"
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+
     GeneratedPluginRegistrant.registerWith(this)
+
+    MethodChannel(flutterView, CHANNEL).setMethodCallHandler { call, result ->
+      if (call.method == "share") {
+        if (call.arguments !is Map<*, *>) {
+          throw IllegalArgumentException("Map argument expected")
+        }
+
+        val text = call.argument<String>("text")
+        share(text!!)
+
+        result.success(true)
+      } else {
+        result.notImplemented()
+      }
+    }
+  }
+
+  private fun share(text: String) {
+    if (text.isEmpty()) {
+      throw IllegalArgumentException("Non-empty text expected")
+    }
+
+    val shareIntent = Intent()
+    shareIntent.setAction(Intent.ACTION_SEND)
+    shareIntent.putExtra(Intent.EXTRA_TEXT, text)
+    shareIntent.setType("text/plain")
+
+    val chooserIntent = Intent.createChooser(shareIntent, null)
+    startActivity(chooserIntent)
   }
 }
--- lib/share/share_video.dart
+++ lib/share/share_video.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:video_example/db/entities.dart';
 
 class ShareScreen extends StatefulWidget {
@@ -11,14 +12,28 @@ class ShareScreen extends StatefulWidget {
 }
 
 class _ShareScreenState extends State<ShareScreen> {
+  static const shareMethodChannel = const MethodChannel('samples.flutter.io/share_file');
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("Share data"),
+          title: Text("Share video"),
         ),
-        body: Center(child: Text(widget.video.date.toString()))
+        body: Center(
+            child: RaisedButton(
+                child: Text("share"),
+                onPressed: () => shareIt()
+            )
+        )
     );
+  }
+
+  shareIt() async {
+    try {
+      await shareMethodChannel.invokeMethod("share", {"text": "Hello, world!"});
+    } on PlatformException catch (e) {
+      print(e);
+    }
   }
 }
